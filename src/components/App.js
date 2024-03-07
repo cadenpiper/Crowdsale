b4be3420604c9d5570c43d15a2539f980ec2e8ce
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-03-07 16:28:11 -0600

    Load Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
 
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
+
+	const [account, setAccount] = useState(null);
+
+	const loadBlockchainData = async () => {
+		const provider = await new ethers.providers.Web3Provider(window.ethereum)
+
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+		const account = ethers.utils.getAddress(accounts[0])
+		setAccount(account)
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	});
+
 	return(
 		<Container>
 			<Navigation />
+			<hr />
+			{account && (
+				<Info account={account} />
+			)}
 		</Container>
 	)
 }
