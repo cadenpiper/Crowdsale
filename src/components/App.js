1e1a35b9f8e8cea10142782c11c0f9a32f11eaf4
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-03-07 00:29:32 -0600

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container } from 'react-bootstrap';
+
+import Navigation from './Navigation';
 
 function App() {
 	return(
-		<div>Hello, world!</div>
+		<Container>
+			<Navigation />
+		</Container>
 	)
 }
 
